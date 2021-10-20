edbabd354aa1a1bb1c69b0458ddc8d037fbe70cf
Flavien <flavien.desse@student.junia.com>
2021-10-20 11:14:22 +0200

    [FDE] end styling

--- src/components/HomePage/style.js
+++ src/components/HomePage/style.js
@@ -2,76 +2,91 @@ import {makeStyles} from "@mui/styles"
 import backgroundImageLogoJunia from "../../image/backgroundConnection.png"
 
 
-export const useStyle = makeStyles((theme)=>({
-    root:{
-        width:"100vw",
-        height:"100vh",
-        backgroundPosition:"left",
-       
-        backgroundImage:"url("+backgroundImageLogoJunia+")",
-        backgroundSize:"cover",
-        backgroundRepeat:"no-repeat",
-        backgroundAttachment:"fixed"
-    },
-    title:{
-       color:"white",
-       fontFamily:"Montserrat !important" ,
-       fontWeight: "bold !important",
-       position:"absolute",
-       top:"30px",
-       left:"30px"
-    },
-    ConnectionTitle:{
-        color:"white",
-        fontFamily:"Montserrat",
-        top:"30px",
-        position:"absolute",
-        right:"30px",
-        display:"flex",
-        flexDirection:"row",
-        alignItems:"center"
-    },
-    iconPerson:{
-        marginRight:"7px"
-    },
-    code:{
-        color:"white",
-        fontFamily:"Montserrat",
-        position:"absolute",
-        left:"10%",
-        top:"50%",
-        display:"flex",
-        flexDirection:"column",
-        alignItems:"flex-end"
+export const useStyle = makeStyles((theme) => ({
+    root: {
+        width: "100vw",
+        height: "100vh",
+        backgroundPosition: "left",
 
+        backgroundImage: "url(" + backgroundImageLogoJunia + ")",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundAttachment: "fixed"
     },
-    textFieldCode:{
+    title: {
+        color: "white",
+        fontFamily: "Montserrat !important",
+        fontWeight: "bold !important",
+        position: "absolute",
+        top: "30px",
+        left: "30px",
+        [theme.breakpoints.down('md')]: {
+            top: "100px",
+        },
+    },
+    ConnectionTitle: {
+        color: "white",
+        fontFamily: "Montserrat",
+        top: "30px",
+        position: "absolute",
+        right: "30px",
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center"
+    },
+    iconPerson: {
+        marginRight: "7px"
+    },
+    code: {
+        transform: "translate(0%, -50%)",
+        color: "white",
+        fontFamily: "Montserrat",
+        position: "absolute",
+        left: "10%",
+        top: "50%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "flex-end",
+        [theme.breakpoints.down('sm')]: {
+            width: "80%",
+            alignItems: "center",
+        },
+    },
+    textFieldCode: {
         "& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
             borderColor: "white"
-          },
-          "&:hover .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
+        },
+        "&:hover .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
             borderColor: "#AAA"
-          },
-          "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
+        },
+        "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
             borderColor: "#AAA"
-          },
-          "& .MuiOutlinedInput-input": {
+        },
+        "& .MuiOutlinedInput-input": {
             color: "#BBB"
-          },
-          "&:hover .MuiOutlinedInput-input": {
+        },
+        "&:hover .MuiOutlinedInput-input": {
             color: "#BBB"
-          },
-          "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-input": {
+        },
+        "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-input": {
             color: "white"
-          },
-          [theme.breakpoints.down('md')]: {
-            width:"80%",
-          },
-          
+        },
+
+
     },
-    formCode:{
-        marginTop:"10px",
+    containerButtonSubmit:{
+        paddingTop:"20px",
+    },
+    submitButton: {
+        [theme.breakpoints.down('sm')]: {
+            width: "80%",
+        },
+        backgroundColor:"#ff5c39 !important",
+        float: "right",
+    },
+    formCode: {
+        marginTop: "10px",
     }
 
-    
+
 }))
